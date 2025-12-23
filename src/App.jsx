32229c20fba7063458c0778c33437f29eff5ba76
Samiullah Alam <asamiullah084@gmail.com>
2025-12-23 12:58:16 +0530

    Add

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Card from './Component/Card'
+
+const App = () => {
+
+  return (
+    <div>
+      <Card user="Spiderman" img="https://images.unsplash.com/photo-1635805737707-575885ab0820?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3BpZGVybWFufGVufDB8fDB8fHww"
+      tech="UI/UX designer" data="55$/h"
+      />
+      <Card user="Ironman" img="https://images.unsplash.com/photo-1657558045738-21507cf53606?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aXJvbm1hbnxlbnwwfHwwfHx8MA%3D%3D"
+        tech="Mern Stack" data="60$/h"
+      />
+      <Card user="Thor" img="https://media.istockphoto.com/id/1147508542/photo/man-in-cosplaying-thor-isolated-on-white-studio-background.jpg?s=1024x1024&w=is&k=20&c=-glvYxELnM2SIfe2A6FmwltVfzpV-V9ApQpnbixH0eI="
+        tech="Java Full Stack" data="50$/h"
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Card.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Card = (props) => {
+    console.log(props)
+
+
+  return (
+      <div className='parent'>
+          <div className='card'>
+              <div className="top">
+                  <button>Avalaible</button>
+                  <p>{props.data}</p>
+              </div>
+              <div>
+                  <img src={props.img} alt="image" />
+              </div>
+              <div className='medium'>
+                  <h1>{ props.user}</h1>
+                  <span>{props.tech }</span>
+                  <a href="">Epic Coders</a>
+              </div>
+              <div className='tech'>
+                  <button>UI</button>
+                  <button>UX</button>
+                  <button>Photoshop</button>
+              </div>
+              <div className='experience'>
+                  <div className='view'>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis architecto nisi minus sit explicabo, expedita dolores pariatur ipsam quaerat! Sit libero atque aut modi sint velit iure, alias eveniet temporibus!</p>
+                  </div>
+              </div>
+              <button>view profile</button>
+          </div>
+      </div>
+  )
+}
+
+export default Card
